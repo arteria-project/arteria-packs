e2ef37ec52d8a6eb9732acbe6a003c7553a827f2
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2015-07-21 15:27:51 +0200

    Remote get_tiles_to_include

--- actions/lib/bcl2fastq.py
+++ actions/lib/bcl2fastq.py
@@ -16,9 +16,8 @@ def main(argv):
     parser.add_argument('--output', required=True, help='Output folder for bcl2fastq (required)',)
 
     # Optional arguments
-    parser.add_argument('--barcode_missmatches',  help='Number of missmatches to allow (0, 1 or 2)',)
-    parser.add_argument('--auto_tiles',  help='Tiles to include.',)
-    parser.add_argument('--manual_tiles',  help='Tiles to include (will override auto_tiles)',)
+    parser.add_argument('--barcode_mismatches',  help='Number of mismatches to allow (0, 1 or 2)',)
+    parser.add_argument('--tiles',  help='Tiles to include.',)
     parser.add_argument('--use_base_mask',  help='Base mask to use.',)
     parser.add_argument('--additional_args',  help='Additional arguments to feed to bcl2fastq',)
 
@@ -30,14 +29,10 @@ def main(argv):
          "--output-dir", args.output]
 
     if args.barcode_missmatches:
-        commandline.append("--barcode-mismatches " + args.barcode_missmatches)
+        commandline.append("--barcode-mismatches " + args.barcode_mismatches)
 
-    # Preferentially use the manually set tiles. Otherwise
-    # fallback to the automatically set tags.
-    if args.manual_tiles:
+    if args.tiles:
         commandline.append("--tiles " + args.manual_tiles)
-    elif args.auto_tiles:
-        commandline.append("--tiles " + args.auto_tiles)
 
     if args.use_base_mask:
         commandline.append("--use_base_mask " + args.use_base_mask)
