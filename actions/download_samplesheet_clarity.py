c6fa510c575d455001eec2e188ce537ffda375c9
PerLundmark <per.lundmark@medsci.uu.se>
2017-03-09 10:38:50 +0100

    Added logging and fixed split bug

--- actions/download_samplesheet_clarity.py
+++ actions/download_samplesheet_clarity.py
@@ -8,17 +8,22 @@ import os
 from tempfile import mkdtemp
 
 class DownloadSamplesheetClarity(Action):
+    """
+    Downloads a samplesheet from Clarity LIMS (instead of Hermes)
+    keyed on the flowcell id.
+    """
+
     def get_processes(self, containers):
         for container in containers:
             placement = container.get_placements()
             artifact = placement[placement.keys()[0]]
-            yield artifact.parent_process_id
+            yield artifact.parent_process.id
 
     def highest_id(self, process_ids):
         highest_proc_number = 0
         highest_proc_type = ''
         for id in process_ids:
-            (proc_type, proc_number) = id.split(str='-')
+            (proc_type, proc_number) = id.split('-')
             if highest_proc_number == 0 or int(proc_number) > highest_proc_number:
                 highest_proc_number = int(proc_number)
                 highest_proc_type = proc_type
@@ -26,7 +31,7 @@ class DownloadSamplesheetClarity(Action):
 
 
     def run(self, flowcell_name):
-        lims = Lims(BASEURI, USERNAME, PASSWORD) #Need credentials on stackstorm server.
+        lims = Lims(BASEURI, USERNAME, PASSWORD) #Need credentials on stackstorm server for genologics package.
         containers = lims.get_containers(name=flowcell_name)
         processes = list(self.get_processes(containers))
         newest_process_id = self.highest_id(processes)
