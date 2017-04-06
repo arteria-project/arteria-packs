2cb8861441425c5ba30780c19af9c9d920f6db28
monikaBrandt <monika.brandt@medsci.uu.se>
2017-04-06 16:30:03 +0200

    Information about sensitive project added
    Information about sensitive projects are required in SUPR. This
    information is given by the operator through a textfile.

--- actions/read_projects_email_file.py
+++ actions/read_projects_email_file.py
@@ -19,8 +19,10 @@ class ReadProjectsEmailFile(Action):
             reader = csv.DictReader(csv_file, delimiter=';')
             for row in reader:
                 project = row['project']
+                sensitive = self.str_to_bool(row['sensitive'])
                 if project in projects_list:
-                    result[project] = row['email']
+                    result[project] = {"email": row['email'],"sensitive": sensitive}
+                    
 
         if len(projects_list) == len(result.keys()):
             self.logger.info("Projects given and projects found in file did match...")
@@ -28,3 +30,11 @@ class ReadProjectsEmailFile(Action):
         else:
             self.logger.error("Projects given and projects found in file did not match!")
             return False, {}
+
+    def str_to_bool(self, string):
+        if string == "True":
+            return True
+        elif string == "False":
+            return False
+        else:
+            raise ValueError("{} is not a valid boolean.".format(string))
